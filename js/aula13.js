e31d83034450cdde2b204ba85664d112b8055429
Tiago Melo <tmf182@gmail.com>
2014-09-30 09:20:09 -0300

    quarto js

--- js/aula13.js
+++ js/aula13.js
@@ -2,8 +2,8 @@ var paises = new Array("Brasil", "Rússia", "china", "India");
 //paises[1]="RÚSSIA";
 //alert(paises[1]);
 
-for (var i = 0; i < paises.length; i++) {
+/*for (var i = 0; i < paises.length; i++) {
 	alert(paises[i]);
-}
+}*/
 
 alert(paises.length);
